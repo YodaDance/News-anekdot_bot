932216d79786f1ad97645f627b5db3c242c2f2b1
Kirill Sidorov <50771295+YodaDance@users.noreply.github.com>
2020-12-19 22:17:43 +0500

    Добавил events join_member и member_remove
    Теперь бот приветствует новых участников сервера и его покидающих в классическом еврейском прекрасном стиле

--- news_anekdotes.py
+++ news_anekdotes.py
@@ -1,23 +1,41 @@
 import discord
 from discord.ext import commands
 from script import anekdot_script
+from discord.utils import get
 import random
 from config import settings
 
 
-bot = commands.Bot(command_prefix = ('Внимание ', 'внимание '))
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix = ('Внимание ', 'внимание '), intents = intents)
 
 
-@bot.command()
-async def привет(ctx):
-    author = ctx.message.author
-    await ctx.send(f'Hello, {author.mention}! Are you ready for a piece of jewish fun?')
+#bot events
+@bot.event
+async def on_ready(): #just give you a message that it works
+    print('Таки я уже и заработал! Что клювом щёлкаешь?\nТаки моя работа и не бесплатная!\n')
 
 
+@bot.event
+async def on_member_join(member):
+    print(f'{member} has joined the server! Sending welcome message...\n')
+    channel = get(member.guild.channels, name="основной")
+    await channel.send(f'Таки и новый гость к нам пожаловал!\nШалом, {member.mention}! Таки ты готов к новой порции маленькой кошерной радости?\n')
+
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} has joined the server! Sending bye message...\n')
+    channel = get(member.guild.channels, name="основной")
+    await channel.send(f'{member.mention} таки и покинул нас. А где мои денюжки? Таки вы мне должны заплатить за выход, как в любом приличном заведении!\n')
+
+
+#bot commands
 anekdots = []
 n_of_page = 1
 @bot.command()
-async def анекдот(ctx):
+async def анекдот(ctx): #pops out a joke
     global anekdots
     global n_of_page
 
@@ -31,7 +49,7 @@ async def анекдот(ctx):
         await ctx.send(anekdots[0].text)
         anekdots.pop(0)
     else:
-        await ctx.send('Nothing left! Find new web-site for fun!')
+        await ctx.send('Анекдоты кончились, дядя! Ну что ты так много и жадно их читаешь!\nА теперь и сам ищи себе эти анекдоты, родной!')
 
-    
+  
 bot.run(settings['token'])
