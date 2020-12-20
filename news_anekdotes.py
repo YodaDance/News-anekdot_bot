ff33d14f9692f31b9450fb292b94af08f6d4680e
Kirill Sidorov <50771295+YodaDance@users.noreply.github.com>
2020-12-20 17:30:49 +0500

    Появилась команда "новости", обновлены комменты
    Появилась команда "внимание новости", которая зпарсит раздел "экономика" сайта VTimes (бывшая уволенная со скандалом команда Коммерсант), выдает главную новость раздела и остальные 5 новостей + ссылки к ним.  Таки евреев больше экономики ничего и не волнует :)
    В самом пуле около 18 новостей, так что вывод их количества сделать несложно.  В других секциях скрипт будет работать с таким же успехом.
    Если хочется выводить новости с главной страницы: нужно лишь закоментить части, связанные с "news_headline" в функции def news_script и из return убрать news_healine. Помимо это final_news_output должен стать только default_news_output.

--- news_anekdotes.py
+++ news_anekdotes.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from script import anekdot_script
+from script import anekdot_script, news_script
 from discord.utils import get
 import random
 from config import settings
@@ -11,7 +11,7 @@ intents.members = True
 bot = commands.Bot(command_prefix = ('Внимание ', 'внимание '), intents = intents)
 
 
-#bot events
+#bot events - при определенных событиях бот будет реагировать и выполнять действия
 @bot.event
 async def on_ready(): #в консоли выдает сообщение, что работает
     print('Таки я уже и заработал! Что клювом щёлкаешь?\nТаки моя работа и не бесплатная!\n')
@@ -20,20 +20,20 @@ async def on_ready(): #в консоли выдает сообщение, что
 @bot.event
 async def on_member_join(member):
     print(f'{member} has joined the server! Sending welcome message...\n')
-    channel = get(member.guild.channels, name="основной")
+    channel = get(member.guild.channels, name="основной") #вставьте название вашего канала
     await channel.send(f'\nТаки и новый гость к нам пожаловал!\nШалом, {member.mention}! Таки ты готов к новой порции маленькой кошерной радости?')
     #ваше любое сообщение по желанию
 
 
 @bot.event
 async def on_member_remove(member): #пользователь уходит с канала или его выгнали
-    print(f'{member} has joined the server! Sending bye message...\n')
-    channel = get(member.guild.channels, name="основной")
+    print(f'{member} has left the server! Sending bye message...\n')
+    channel = get(member.guild.channels, name="основной") #вставьте название вашего канала для вывода сообщения
     await channel.send(f'\n{member.mention} таки и покинул нас. А где мои денюжки? Таки вы мне должны заплатить за выход, как в любом приличном заведении!')
     #ваше любое сообщение по желанию
 
 
-#bot commands
+#bot commands - команды в чате
 anekdots = []
 n_of_page = 1
 @bot.command()
@@ -53,5 +53,27 @@ async def анекдот(ctx): #будет выводить анекдот, по
     else:
         await ctx.send('Анекдоты кончились, дядя! Ну что ты так много и жадно их читаешь!\nА теперь и сам ищи себе эти анекдоты, родной!')
         #анекдоты могут и кончится :)
-  
-bot.run(settings['token']) #у меня создан отдельный файл с конфигом, сюда вы можете просто вставить свой токен
+
+
+@bot.command()
+async def новости(ctx): #напишет в чат основную новость и 5 других, отображаемые на сайте VTimes
+                        # вы можете сделать больше,чем 5. Новостей около 18 в пулле будет(главная + 17 обычных)
+    news_headline, news_default = news_script() #создаем главную и другие новости в виде словарей
+    news_headline_key = list(news_headline.keys())[0] #сама главная новость, заодно ключ для ссылки
+    news_headline_href = '<{}>'.format(news_headline[news_headline_key]) #чтобы embed не выскакивал при выскакивании ссылок
+    
+
+    news_headline_output = f'***Внимание, новости!\n\n\nГлавная новость:***\n{"**" + news_headline_key + "**"}.\nСсылка к прочтению:\n{news_headline_href}\n\n\n'
+
+
+    default_news_output = '***Перейдем к другим новостям:***\n'
+    for i in range(5): #аналогично как и с главной новостью, только теперь в цикле
+        news_default_key = list(news_default.keys())[i]
+        news_default_href = '<{}>'.format(news_default[news_default_key])
+        default_news_output += f'{i+1}) {"**" + news_default_key + "**" }.\nСсылка к прочтению:\n{news_default_href}\n\n'
+
+    
+    final_news = news_headline_output + default_news_output #сводим главную и другие новости в 1 сообщение
+    await ctx.send(final_news) #отправляем сообщение
+
+bot.run(settings['token']) #у меня создан отдельный файл с конфигом, сюда в ы можете просто вставить свой токен
--- script.py
+++ script.py
@@ -20,3 +20,23 @@ def anekdot_script(anekdots, n_of_page): #return list of anekdots of a specific 
     
     return anekdots, n_of_page
 
+
+
+def news_script(): #возвращает два словаря (главная и другие новости), ключ - сама новость, значение - ссылка на новость. Сайтй - VTimes, экономика и финансы
+                   #вы вполне можете сменить тематику, просто изменив URL VTimes, парсинг будет работать и с ними.
+    response = requests.get('https://www.vtimes.io/economy') 
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    #работает только на тематических разделах, на главном разделе такого класса нет, а вот класс дефолтных новостей есть,так что если настроите на главную страницу - вырезайте/комменьте эту часть кода
+    soup_news_headline = soup.select('.article-excerpt-lead__link')
+    news_headline = dict()
+    news_headline[soup_news_headline[0]['title']] = soup_news_headline[0]['href']
+
+
+    soup_news_default = soup.select(".article-excerpt-default__link")
+    news_default = dict()
+    for new in soup_news_default:
+        news_default[new['title']] = new['href']
+
+
+    return news_headline, news_default
